d76f7dd8ef9b0208cf32f06a1cb339c60b6ca91b
automationgeek <jf.tremblay@gmail.com>
2021-01-29 14:12:43 -0500

    Update unifi_poly.py

--- unifi_poly.py
+++ unifi_poly.py
@@ -166,7 +166,7 @@ class NetDevice(polyinterface.Node):
         
     def update(self):
         try :
-            self.unifiCtrl.get_client(quote(self.deviceMac)) 
+            self.unifiCtrl.get_client('f09fc2301533') 
             self.setDriver('GV1',1)
         except Exception as ex :
             self.setDriver('GV1',0)
